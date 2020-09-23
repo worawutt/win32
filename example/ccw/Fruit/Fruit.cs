19a566e3e5f9b8133329cb91cf882e7e5c9c585a
Tim Sneath <timsneath@google.com>
2020-09-23 15:39:44 -0700

    Simple COM-callable class

---
+++ example/ccw/Fruit/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public interface IFruit
+{
+    public int SeedCount { get; set; }
+    public string Species { get; set; }
+
+    public string Eat();
+    public List<IFruit> Reproduce();
+    public bool CanMakeGoodWine();
+}
+
+[ClassInterface(ClassInterfaceType.AutoDual)]
+public class Apple : IFruit
+{
+    public int SeedCount { get; set; }
+    public string Species { get; set; }
+
+    public bool CanMakeGoodWine() => false;
+
+    public string Eat()
+    {
+        Console.WriteLine("This apple is very tasty.");
+        return "Apple eaten.";
+    }
+
+    public List<IFruit> Reproduce()
+    {
+        return new List<IFruit>() { this, (IFruit) MemberwiseClone() };
+    }
+}
+
